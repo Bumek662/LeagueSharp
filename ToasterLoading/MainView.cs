85733d23f158218246efcb6c28af48dcba130e66
Bumek662 <piotrsawicki2000@gmail.com>
2015-01-09 18:31:39 +0100

    Toaster Loading
    Added

---
+++ ToasterLoading/MainView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ToasterLoading
+{
+    public partial class MainView : Form
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
